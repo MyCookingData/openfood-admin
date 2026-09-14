9b641694e8338e92e77263452bc2c4e439ac7b9e
Ugo Aubri <ugoaubri@gmail.com>
2026-09-14 16:37:28 -0400

    fix(firebase): link to client project open-food-app-819c2

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ void main() async {
     // Initialisation de Firebase avec vos véritables clés de projet
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: 'AIzaSyA49Q5B4nF8K2OZtDoxUsmELpAb0zZUZbc',
-        appId: '1:178517740611:web:09dfba077107c07f59d8f6',
-        messagingSenderId: '178517740611',
-        projectId: 'app-openfood',
-        authDomain: 'app-openfood.firebaseapp.com',
-        storageBucket: 'app-openfood.firebasestorage.app',
+        apiKey: 'AIzaSyCaAfkg8zLlK8gU42rDJ4zeieZ4xXtXp-k',
+        appId: '1:809394357365:web:60927b7eb4a158e23f643c',
+        messagingSenderId: '809394357365',
+        projectId: 'open-food-app-819c2',
+        authDomain: 'open-food-app-819c2.firebaseapp.com',
+        storageBucket: 'open-food-app-819c2.firebasestorage.app',
       ),
     );
   } catch (e) {
